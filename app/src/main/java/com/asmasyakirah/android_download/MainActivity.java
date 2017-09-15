776827fdb3432e984c7d81bef4b43a9ba41827e2
ASMA-NB\Asma' Syakirah <asma.syakirahd@gmail.com>
2017-09-15 10:47:39 +0800

    Created option 1 (Intent Service) activity.

--- app/src/main/java/com/asmasyakirah/android_download/MainActivity.java
+++ app/src/main/java/com/asmasyakirah/android_download/MainActivity.java
@@ -1,14 +1,34 @@
 package com.asmasyakirah.android_download;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity
 {
+    Intent intent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToDownload(View view)
+    {
+        // Get button tag
+        int tag = Integer.parseInt(view.getTag().toString());
+
+        switch (tag)
+        {
+            case 0:
+                // Intent Service
+                intent = new Intent(this, IntentServiceActivity.class);
+                break;
+        }
+
+        startActivity(intent);
     }
 }
